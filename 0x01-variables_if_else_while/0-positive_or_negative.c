837398535122a910ddb857f7393ed538199f52e5
Dharnear <dharnearspark@gmail.com>
2022-10-14 09:16:50 -0700

    c-var_if commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,14 +18,12 @@ int main(void)
 	{	
 		printf("%i is positive\n", n);
 	}
-
 	else if (n == 0)
 	{
 		printf("%i is zero\n", n);
 	}
-
 	else if (n < 0)
-	{							
+	{						
 		printf("%i is negative\n", n);
 	}
 
