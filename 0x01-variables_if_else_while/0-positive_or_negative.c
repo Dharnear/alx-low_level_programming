d1524bf476543b6906787ac26b9c6f0d7b170fa6
Dharnear <dharnearspark@gmail.com>
2022-10-14 09:24:28 -0700

    C-var_if_else commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{	
+	{
 		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
@@ -23,9 +23,9 @@ int main(void)
 		printf("%i is zero\n", n);
 	}
 	else if (n < 0)
-	{						
+	{
 		printf("%i is negative\n", n);
 	}
 
-	return(0);
+	return (0);
 }
