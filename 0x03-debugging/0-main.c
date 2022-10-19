43d2855b23d78640fad761beb0ad6befdd2f072b
Dharnear <dharnearspark@gmail.com>
2022-10-19 09:37:42 -0700

    multi_mains commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+
+	return (0)
+
+}
